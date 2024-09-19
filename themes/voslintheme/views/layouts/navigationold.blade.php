c20affd6e33ec4956798fc5f34b69c8ac50f3d55
systemadmin <sonic.casanova.hedgehog@gmail.com>
2024-09-20 01:25:38 +0200

    fixed missing support buttons and icons in headermenu

--- themes/voslintheme/views/layouts/navigationold.blade.php
+++ themes/voslintheme/views/layouts/navigationold.blade.php
@@ -18,26 +18,26 @@
                 }
             </script>
         </div>
-        <div class="w-full justify-between gap-x-10 md:px-5 md:flex hidden" id="mobile-menu">
+        <div class="w-full justify-between gap-x-5 md:px-5 md:flex hidden" id="mobile-menu">
             <a href="{{ route('index') }}"
                 class="md:px-2 py-3 flex items-center gap-x-1 hover:text-secondary-800 duration-300">
                 <i class="ri-home-4-line"></i> {{ __('Home') }}
             </a>
-            @auth
+            @auth 
                 <a href="{{ route('clients.home') }}"
                     class="md:px-2 py-3 flex items-center gap-x-1 hover:text-secondary-800 duration-300">
-                    {{ __('Customer Area') }}
+                    <i class="ri-dashboard-line"></i> {{ __('Dashboard') }}
                 </a>
             @endauth
             <button type="button" aria-expanded="true" data-dropdown-placement="bottom-start" aria-haspopup="true"
                 data-dropdown-toggle="orders"
                 class="relative md:px-2 py-3 flex items-center gap-x-1 hover:text-secondary-800 duration-300">
-                <i class="ri-server-line"></i> {{ __('Shop') }} <i class="ri-arrow-down-s-line"></i>
+                <i class="ri-server-line"></i> {{ __('Services') }} <i class="ri-arrow-down-s-line"></i>
 
                 <div class="absolute left-0 hidden w-56 mt-2 origin-top-right bg-secondary-200 border border-secondary-300 rounded-md z-10"
                     role="menu" aria-orientation="vertical" aria-labelledby="product" tabindex="-1" id="orders">
-                    @foreach (App\Models\Category::whereNull('category_id')->orderBy('order')->get() as $category)
-                        @if ($category->products()->where('hidden', false)->count() > 0 || $category->children->count() > 0)
+                    @foreach (App\Models\Category::withCount('products')->orderBy('order')->get() as $category)
+                        @if ($category->products_count > 0)
                             <a href="{{ route('products', $category->slug) }}"
                                 class="flex px-4 py-2 rounded text-secondary-700 hover:bg-secondary-100 hover:text-secondary-900"
                                 role="menuitem" tabindex="-1" id="menu-item-0">{{ $category->name }}</a>
@@ -48,9 +48,14 @@
             </button>
             <a href="{{ route('announcements.index') }}"
                 class="md:px-2 py-3 flex items-center gap-x-1 hover:text-secondary-800 duration-300">
-                <i class="ri-megaphone-line @if (request()->routeIs('announcements.index')) text-primary-400 @endif"></i>{{ __('Announcements') }}
+                <i class="ri-megaphone-line @if (request()->routeIs('announcements.index')) text-primary-400 @endif"></i> {{ __('Announcements') }}
             </a>
-
+            @if(config('settings::theme:panel') == 1)
+            <a href="{{ config('settings::theme:panel-url', '#') }}"
+                class="md:px-2 py-3 flex items-center gap-x-1 hover:text-secondary-800 duration-300">
+                <i class="ri-window-line"></i> {{ __('Panel') }}
+            </a>
+            @endif
             <button type="button" aria-expanded="true" data-dropdown-placement="bottom-start" aria-haspopup="true"
                 data-dropdown-toggle="support"
                 class="relative md:px-2 py-3 flex items-center gap-x-1 hover:text-secondary-800 duration-300">
@@ -74,27 +79,48 @@
                 </div>
 
             </button>
-
             <div class="ml-auto flex items-center gap-x-1 justify-center md:pb-0 pb-4">
-                <livewire:cart-count />
+                @if (count(session()->get('cart', [])) > 0)
+                    <a href="{{ route('checkout.index') }}" class="button button-secondary-outline !font-normal">
+                        <i class="ri-shopping-cart-line"></i>
+                        {{ count(session()->get('cart')) }}
+                    </a>
+                @endif
+                @if (config('settings::sidebar') == 0)
+                    <button class="py-1 float-right button button-secondary-outline !font-normal" id="theme-toggle">
+                        <i class="ri-sun-line hidden dark:block"></i>
+                        <i class="ri-moon-line dark:hidden"></i>
+                    </button>
+                    <script>
+                        // Change the icons inside the button based on previous settings
+                        if (localStorage.getItem('theme') === 'dark' || (!('theme' in localStorage) && window.matchMedia(
+                            '(prefers-color-scheme: dark)').matches));
 
+                        var themeToggleBtn = document.getElementById('theme-toggle');
+                        themeToggleBtn.addEventListener('click', function() {
+                            // if set via local storage previously
+                            if (localStorage.getItem('theme')) {
+                                if (localStorage.getItem('theme') === 'light') {
+                                    document.documentElement.classList.add('dark');
+                                    localStorage.setItem('theme', 'dark');
+                                } else {
+                                    document.documentElement.classList.remove('dark');
+                                    localStorage.setItem('theme', 'light');
+                                }
+                                // if NOT set via local storage previously
+                            } else {
+                                if (document.documentElement.classList.contains('dark')) {
+                                    document.documentElement.classList.remove('dark');
+                                    localStorage.setItem('theme', 'light');
+                                } else {
+                                    document.documentElement.classList.add('dark');
+                                    localStorage.setItem('theme', 'dark');
+                                }
+                            }
+                        });
+                    </script>
+                @endif
                 @auth
-                    @if(Auth::user()->credits > 0 && config('settings::credits'))
-                        <div class="ml-auto items-center justify-end hidden md:flex">
-                            <a href="{{ route('clients.credits') }}" class="text-md mr-2">
-                                <i class="ri-wallet-3-line"></i> {{__('Your Balance:')}} <span class="font-semibold">
-                                    <x-money :amount="Auth::user()->credits" />
-                                </span>
-                            </a>
-                        </div>
-                        <div class="ml-auto flex items-center justify-end md:hidden">
-                            <a href="{{ route('clients.credits') }}" class="text-md mr-2">
-                                <i class="ri-wallet-3-line"></i> <span class="font-semibold">
-                                    <x-money :amount="Auth::user()->credits" />
-                                </span>
-                            </a>
-                        </div>
-                    @endif
                     <button type="button" aria-expanded="true" aria-haspopup="true" data-dropdown-placement="bottom-end"
                             data-dropdown-toggle="account" class="relative md:flex-none flex-1">
                         <div class="inline-flex items-center justify-center">
@@ -110,10 +136,19 @@
                                 <a href="{{ route('clients.profile') }}" class="px-2 py-2 hover:bg-secondary-300 flex items-center gap-x-2 rounded transition-all ease-in-out">
                                     <i class="ri-account-circle-line"></i> {{__('Profile')}}
                                 </a>
+                                <a href="{{ route('clients.credits') }}" class="px-2 py-2 hover:bg-secondary-300 flex items-center gap-x-2 rounded transition-all ease-in-out">
+                                    <i class="ri-money-dollar-circle-line"></i> {{__('Credit')}}
+                                </a>
+                                <a href="{{ route('clients.home') }}" class="px-2 py-2 hover:bg-secondary-300 flex items-center gap-x-2 rounded transition-all ease-in-out">
+                                    <i class="ri-dashboard-line"></i> {{__('Dashboard')}}
+                                </a>
 
                                 {{-- <a href="#" class="px-2 py-2 hover:bg-secondary-300 flex items-center gap-x-2 rounded"><i class="ri-instance-line"></i> {{ __('Services') }}</a> --}}
 
                                 @if (Auth::user()->has('ADMINISTRATOR'))
+
+                                    <hr class="mx-2 my-1 border-secondary-400" />
+
                                     <a href="{{ route('admin.index') }}" class="px-2 py-2 hover:bg-secondary-300 flex items-center gap-x-2 rounded transition-all ease-in-out">
                                         <i class="ri-key-2-line"></i> {{ __('Admin area') }}
                                     </a>
@@ -140,38 +175,6 @@
                         {{ __('Log In') }}
                     </a>
                 @endauth
-                <button class="m-1.5 button button-secondary-outline !font-normal" id="theme-toggle">
-                    <i class="ri-sun-line hidden dark:block"></i>
-                    <i class="ri-moon-line dark:hidden"></i>
-                </button>
-                <script>
-                    // Change the icons inside the button based on previous settings
-                    if (localStorage.getItem('theme') === 'dark' || (!('theme' in localStorage) && window.matchMedia(
-                        '(prefers-color-scheme: dark)').matches));
-
-                    var themeToggleBtn = document.getElementById('theme-toggle');
-                    themeToggleBtn.addEventListener('click', function() {
-                        // if set via local storage previously
-                        if (localStorage.getItem('theme')) {
-                            if (localStorage.getItem('theme') === 'light') {
-                                document.documentElement.classList.add('dark');
-                                localStorage.setItem('theme', 'dark');
-                            } else {
-                                document.documentElement.classList.remove('dark');
-                                localStorage.setItem('theme', 'light');
-                            }
-                            // if NOT set via local storage previously
-                        } else {
-                            if (document.documentElement.classList.contains('dark')) {
-                                document.documentElement.classList.remove('dark');
-                                localStorage.setItem('theme', 'light');
-                            } else {
-                                document.documentElement.classList.add('dark');
-                                localStorage.setItem('theme', 'dark');
-                            }
-                        }
-                    });
-                </script>
             </div>
         </div>
     </div>
